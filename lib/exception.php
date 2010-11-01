36f195ecdd3b8a1fb6e4c1acd02d6b9f1b387656
connec <connec.2002@gmail.com>
2010-11-01 00:53:02 +0000

    Extended hamlparser\lib\Exception with a system for changing variables after construction.

--- lib/exception.php
+++ lib/exception.php
@@ -20,6 +20,16 @@ class Exception extends \Exception {
 	protected static $messages = array();
 	
 	/**
+	 * The original, non-substituted message.
+	 */
+	protected $original_message;
+	
+	/**
+	 * An array of variables to substitute into the message on string conversion.
+	 */
+	protected $variables = array();
+	
+	/**
 	 * Sets the message.
 	 * 
 	 * @param string $message
@@ -31,11 +41,39 @@ class Exception extends \Exception {
 		if(isset(static::$messages[$message]))
 			$message = static::$messages[$message];
 		
+		$this->original_message = $message;
+		$this->variables = $var;
+		
+		$this->substitute();
+		
+	}
+	
+	/**
+	 * Substitutes set variables into the original message.
+	 * 
+	 * @return void
+	 */
+	protected function substitute() {
+		
 		$this->message = str_replace(
-			array_map(function($k) { return ':' . $k; }, array_keys($vars)),
-			array_values($vars),
-			$message
+			array_map(function($v) { return ':' . $v; }, array_keys($this->variables)),
+			array_values($this->variables),
+			$this->original_message
 		);
+		
+	}
+	
+	/**
+	 * Sets a variable and resubstitutes into the original message.
+	 * 
+	 * @param string $var
+	 * @param mixed  $val
+	 * @return void
+	 */
+	public function set($var, $val) {
+		
+		$this->variables[$var] = $val;
+		$this->substitute();
 		
 	}
 	
